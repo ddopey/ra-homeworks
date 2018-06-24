62ab2a40cb2645db506713db2b10261a1eeb1a80
ddopey <dopey@inbox.ru>
2018-06-24 16:24:32 +0300

    изменен компонент RadioGroup

--- typechecking/components-doc/js/RadioGroup.js
+++ typechecking/components-doc/js/RadioGroup.js
@@ -16,3 +16,12 @@ const RadioGroup = props => {
     </div>
   )
 };
+
+RadioGroup.propTypes = {
+  onChange: PropTypes.func.isRequired,
+
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired, 
+  list: PropTypes.arrayOf(PropTypes.oneOf(['Male', 'Female'])).isRequired,
+}
