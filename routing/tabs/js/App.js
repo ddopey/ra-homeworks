2fb63cda1a147b6b117dba5baf45fbe251f1fec4
ddopey <dopey@inbox.ru>
2018-07-02 23:34:37 +0300

    App.js is modified

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,24 @@
+const activeClassName = 'tabs__item-active';
 
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+         <div className="tabs">
+            <nav className="tabs__items">
+              <NavLink className="tabs__item" activeClassName={activeClassName} exact to="/" >Рефераты</NavLink>
+              <NavLink className="tabs__item" activeClassName={activeClassName} to="/creator">Криэйтор</NavLink>
+              <NavLink className="tabs__item" activeClassName={activeClassName} to="/fortune">Гадалка</NavLink>
+            </nav>
+            <div className="tabs__content">
+              <Switch>
+                  <Route path="/creator" component={Creator} />
+                  <Route path="/fortune"  component={Fortune} /> 
+                  <Route path="/" exact component={Essay} />
+              </Switch>
+            </div>
+          </div>
+      </Router>
+    );
+  }
+}
